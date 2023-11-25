be6b04129b2381d84c638e801fffd631ccf64a71
Lancelot <bulanlancelot@gmail.com>
2023-11-26 00:23:40 +0800

    LOGIN REGISTER COMPLETE AUTH

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,17 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.ics26011_mp5"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.ics26011_mp5"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -33,11 +34,17 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("com.google.firebase:firebase-analytics")
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-database")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/example/ics26011_mp5/MainActivity.kt
+++ app/src/main/java/com/example/ics26011_mp5/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.ics26011_mp5
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.core.widget.ContentLoadingProgressBar
+import com.example.ics26011_mp5.databinding.ActivityMainBinding
+import com.example.ics26011_mp5.databinding.ActivitySignupBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var button: Button
+    lateinit var loadingProgress: ProgressBar
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        // Retrieve user information from Intent
+        val userName = intent.getStringExtra("USERNAME")
+        val userEmail = intent.getStringExtra("EMAIL")
+        button = binding.logoutBtn
+        loadingProgress = binding.progressBar
+
+        val username: TextView = findViewById(R.id.slogan_name)
+        username.text = "Your name is: $userName"
+        val email: TextView = findViewById(R.id.slogan_name2)
+        email.text = "Your email is: $userEmail"
+
+        button.setOnClickListener {
+            button.visibility = View.INVISIBLE
+            loadingProgress.visibility = View.VISIBLE
+            Firebase.auth.signOut()
+
+            val intent = Intent(this, Login::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
